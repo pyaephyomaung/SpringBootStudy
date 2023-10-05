d3cc1557848e8a82ab3ad1ae52e67d608596b7e4
pyaephyomaung <samuelfruit@gmail.com>
2023-10-05 18:39:06 +0900

    05.SpringBootThymeleafIntegration

---
+++ 05.SpringBootThymeleafIntegration/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping({"","/","/home"})
+    public String goToHomePage(Model model){
+        model.addAttribute("username","Maung Maung");
+        return "home.html";
+    }
+}
